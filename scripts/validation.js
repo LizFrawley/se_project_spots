cc33f9fdaa2c21a6383f1b72ce8e11338800b067
Elizabeth Frawley <efrawley529@icloud.com>
2025-06-24 20:11:45 -0400

    add toggleButtonState and css styles for disabled button

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,7 +1,13 @@
 const showInputError = (formEl, inputEl, errorMsg) => {
-  const errorMsgID = inputEl.id + "-error";
-  const errorMsgEl = document.querySelector("#" + errorMsgID);
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
   errorMsgEl.textContent = errorMsg;
+  inputEl.classList.add("modal__input_type_error");
+};
+
+const hideInputError = (formEl, inputEl) => {
+  const errorMsgEl = formEl.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = "";
+  inputEl.classList.remove("modal__input_type_error");
 };
 
 const checkInputValidity = (formEl, inputEl) => {
@@ -12,18 +18,33 @@ const checkInputValidity = (formEl, inputEl) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((input) => {
+    return !input.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonEl) => {
+  if (hasInvalidInput(inputList)) {
+    buttonEl.disabled = true;
+    buttonEl.classList.add("modal__submit-button_disabled");
+  } else {
+    buttonEl.disabled = false;
+    buttonEl.classList.remove("modal__submit-button_disabled");
+  }
+};
+
 const setEventListeners = (formEl) => {
   const inputList = Array.from(formEl.querySelectorAll(".modal__input"));
-  const buttonEl = formEl.querySelector(".modal__submit-btn");
-
+  const buttonEl = formEl.querySelector(".modal__submit-button");
   //TODO: handle initial states
 
-  //toggleButtonState(inputList, buttonEl);
+  toggleButtonState(inputList, buttonEl);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", function () {
       checkInputValidity(formEl, inputElement);
-      //toggleButtonState(inputList, buttonEl);
+      toggleButtonState(inputList, buttonEl);
     });
   });
 };
